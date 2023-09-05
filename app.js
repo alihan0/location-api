3ccbeacc4dda3686daa3dcd1c31bb0f8b68645f3
Alihan Öztürk <alihanozturk364@gmail.com>
2023-09-05 10:10:42 +0300

    id filter created to states

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const ctires = [{id: 25, name : "City1", state : 32}];
 
 app.use(express.json());
 
+// ALL COUNTRIES
 app.get('/country/all', (req, res) => {
     db.query('SELECT * FROM countries', (err, results) => {
         if(err){
@@ -23,6 +24,8 @@ app.get('/country/all', (req, res) => {
     });
 });
 
+
+// COUNTRIES FILTER BY ID
 app.get('/country/id/:id', (req, res) => {
     const countryID = parseInt(req.params.id);
    
@@ -37,7 +40,8 @@ app.get('/country/id/:id', (req, res) => {
     
 });
 
-app.get('/country/slug/:slug', (req, res) => {
+// COUNTRIES FILTER BY COUNTRY CODE
+app.get('/country/code/:slug', (req, res) => {
     db.query('SELECT * FROM countries WHERE iso2 = ?', [req.params.slug], (err, result) => {
         if(err){
             console.error('Veritabanı hatası oluştu: ', err);
@@ -47,6 +51,31 @@ app.get('/country/slug/:slug', (req, res) => {
         res.json(result);
     });
 });
+
+// ALL STATES
+app.get('/state/all', (req, res) => {
+    db.query('SELECT * FROM states', (err, result) => {
+        if(err){
+            console.error("Veritabanı hatası oluştu: ", err);
+            res.status(500).json({error: "Veritabanı hatası!"});
+            return;
+        }
+        res.json(result);
+    });
+});
+
+// STATEES FILTER BY ID
+app.get('/state/id/:id', (req, res) => {
+    db.query('SELECT * FROM states WHERE id = ?', [req.params.id], (err, result) => {
+        if(err){
+            console.error('Veritabanı hatası: ', err);
+            res.status(500).json({error : "veritabanı hatası!"});
+            return;
+        }
+        res.json(result);
+    })
+});
+
 
 app.listen(PORT, () => {
     console.log(`Sunucu ${PORT} portunda çalışıyor...`);
